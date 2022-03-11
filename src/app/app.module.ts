09d8cf7b8151bf6d8b063715e14e5794c03c4d9e
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2022-03-11 17:33:33 +0900

    leaflet 、 leaflet.markercluster をインポート。CSSなど参照設定を追加。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapLayerComponent } from './map-layer/map-layer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapLayerComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    LeafletModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
